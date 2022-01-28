cd1bace3190242fa3e76d6b71c32344043731bb9
comalley29 <charliejohn2004@gmail.com>
2022-01-28 11:35:55 +0000

    added maze styles to list

--- progproject/mazes/forms.py
+++ progproject/mazes/forms.py
@@ -9,8 +9,17 @@ Maze_Styles =(
     ("1", "Aldousbroder"),
     ("2", "Backtracking"),
     ("3", "Binary-Tree"),
-    ("4", ""),
-    ("5", ""),
+    ("4", "Cellular-Automation "),
+    ("5", "Dungeon-Rooms"),
+    ("6", "Ellers"),
+    ("7", "Growing-Tree"),
+    ("8", "Hunt-and-Kill"),
+    ("9", "Kruskals"),
+    ("10", "Prims"),
+    ("11", "Recursive-Division"),
+    ("12", "Sidewinder"),
+    ("13", "Wilsons"),
+
 )
 
 class MazeInputs(forms.Form):
